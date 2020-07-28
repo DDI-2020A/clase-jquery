b153c904a639bcc38dbcec9cf1e88b7748916471
chalosalvador <chalosalvador@gmail.com>
2020-07-28 07:48:27 -0500

    Renderizado de arreglo

--- js/main.js
+++ js/main.js
@@ -1,65 +1,188 @@
 const data = {
-  'first_name': 'Kamryn',
-  'last_name': 'Krajcik',
-  'photo': 'https://s3.amazonaws.com/uifaces/faces/twitter/jordyvdboom/128.jpg',
-  'email': 'Garnet20@yahoo.com',
-  'title': 'Dynamic Optimization Producer',
-  'job_type': 'Associate',
-  'telephone': [
-    '481-848-0356 x164',
-    '920-438-6492 x8487',
-    '1-957-980-5197 x38903'
-  ],
-  'address': {
-    'zip_code': '69880',
-    'street': 'Willms Ridge',
-    'city': 'North Aldaside',
-    'country': 'Turkey'
-  },
-  'friends': [
+  'users': [
     {
-      'first_name': 'Jaquan',
-      'last_name': 'Oberbrunner',
-      'email': 'Marjory5@gmail.com'
+      'first_name': 'Rosalyn',
+      'last_name': 'Weissnat',
+      'photo': 'https://s3.amazonaws.com/uifaces/faces/twitter/lokesh_coder/128.jpg',
+      'email': 'Sven78@gmail.com',
+      'title': 'National Accountability Executive',
+      'job_type': 'Associate',
+      'telephone': [
+        '(846) 707-9757 x60138',
+        '588-543-7408 x5964',
+        '(299) 665-2020'
+      ],
+      'address': {
+        'zip_code': '42365-4384',
+        'street': 'Clark Mission',
+        'city': 'Port Houstonstad',
+        'country': 'Comoros'
+      },
+      'friends': [
+        {
+          'first_name': 'Delfina',
+          'last_name': 'Hills',
+          'email': 'Meredith.Hartmann28@gmail.com'
+        },
+        {
+          'first_name': 'Damaris',
+          'last_name': 'Torphy',
+          'email': 'Bernard_Schuppe97@hotmail.com'
+        }
+      ]
     },
     {
-      'first_name': 'Brigitte',
-      'last_name': 'Ondricka',
-      'email': 'Hermann.Hickle@gmail.com'
+      'first_name': 'Vidal',
+      'last_name': 'Harris',
+      'photo': 'https://s3.amazonaws.com/uifaces/faces/twitter/andytlaw/128.jpg',
+      'email': 'Savion11@gmail.com',
+      'title': 'Customer Security Strategist',
+      'job_type': 'Assistant',
+      'telephone': [
+        '307-308-6553 x649',
+        '1-474-217-5317',
+        '276.775.1889'
+      ],
+      'address': {
+        'zip_code': '04545-0936',
+        'street': 'Tyree Parkway',
+        'city': 'Port Jazminview',
+        'country': 'Namibia'
+      },
+      'friends': [
+        {
+          'first_name': 'Stella',
+          'last_name': 'Fay',
+          'email': 'Krystal_Sanford@hotmail.com'
+        },
+        {
+          'first_name': 'Eleanore',
+          'last_name': 'Cassin',
+          'email': 'Ilene_McLaughlin24@yahoo.com'
+        }
+      ]
+    },
+    {
+      'first_name': 'Alek',
+      'last_name': 'Jerde',
+      'photo': 'https://s3.amazonaws.com/uifaces/faces/twitter/hgharrygo/128.jpg',
+      'email': 'Audie_Howell@yahoo.com',
+      'title': 'Future Marketing Associate',
+      'job_type': 'Analyst',
+      'telephone': [
+        '466-481-7690 x7055',
+        '415.594.6549 x4264',
+        '308.254.4655'
+      ],
+      'address': {
+        'zip_code': '67679-0561',
+        'street': 'Fritz Vista',
+        'city': 'East Darrylborough',
+        'country': 'Angola'
+      },
+      'friends': [
+        {
+          'first_name': 'Benton',
+          'last_name': 'Kris',
+          'email': 'Tiana9@yahoo.com'
+        },
+        {
+          'first_name': 'Pasquale',
+          'last_name': 'Ondricka',
+          'email': 'Chandler.Marvin55@hotmail.com'
+        }
+      ]
+    },
+    {
+      'first_name': 'Abdul',
+      'last_name': 'Cruickshank',
+      'photo': 'https://s3.amazonaws.com/uifaces/faces/twitter/BroumiYoussef/128.jpg',
+      'email': 'Bella31@yahoo.com',
+      'title': 'Future Branding Liaison',
+      'job_type': 'Administrator',
+      'telephone': [
+        '1-631-664-2089 x460',
+        '1-572-662-7412 x214',
+        '436.357.1653'
+      ],
+      'address': {
+        'zip_code': '63518',
+        'street': 'Kub Cove',
+        'city': 'Aufderharberg',
+        'country': 'Andorra'
+      },
+      'friends': [
+        {
+          'first_name': 'Waylon',
+          'last_name': 'Wyman',
+          'email': 'Bo61@gmail.com'
+        },
+        {
+          'first_name': 'Candido',
+          'last_name': 'Walker',
+          'email': 'Stanley.Bartoletti@gmail.com'
+        }
+      ]
+    },
+    {
+      'first_name': 'Elva',
+      'last_name': 'Koelpin',
+      'photo': 'https://s3.amazonaws.com/uifaces/faces/twitter/colirpixoil/128.jpg',
+      'email': 'Bertha_Mann@gmail.com',
+      'title': 'Legacy Assurance Coordinator',
+      'job_type': 'Assistant',
+      'telephone': [
+        '629.397.6601 x96769',
+        '(553) 220-6124',
+        '1-781-712-7258 x7734'
+      ],
+      'address': {
+        'zip_code': '44480-0137',
+        'street': 'Steuber Springs',
+        'city': 'North Lillian',
+        'country': 'Bangladesh'
+      },
+      'friends': [
+        {
+          'first_name': 'Demario',
+          'last_name': 'Metz',
+          'email': 'Jonas77@hotmail.com'
+        },
+        {
+          'first_name': 'Xander',
+          'last_name': 'Aufderhar',
+          'email': 'Mireya_Williamson@yahoo.com'
+        }
+      ]
     }
   ]
 };
 
 $( document ).ready( function() {
 
-  // $( '#users-list' ).html( '' +
-  //   '<div class="media">' +
-  //   '  <div class="img-wrapper">' +
-  //   '     <img src="' + data.photo + '" class="mr-3" alt="...">' +
-  //   '    </div>' +
-  //   '    <div class="media-body">' +
-  //   '    <h5 class="mt-0">' + data.first_name + ' ' + data.last_name + '</h5>' +
-  //   '<strong>Email: </strong>' + data.email +
-  //   '  </div>' +
-  //   '</div>' );
+  let usersHTML = '';
 
-  $( '#users-list' ).html( `
+  data.users.forEach( ( user ) => {
+    usersHTML += `
 <div class='media'>
     <div class='img-wrapper'>
-        <img src='${ data.photo }' class='mr-3' alt='...'>
+        <img src='${ user.photo }' class='mr-3' alt='...'>
     </div>
     <div class='media-body'>
-        <h5 class='mt-0'>${ data.first_name } ${ data.last_name }</h5>
-        <strong>Email: </strong>${ data.email }
-        <button type='button' class='btn btn-secondary' data-toggle='tooltip' data-placement='top' title='${ data.address.street }, ${ data.address.city }, ${ data.address.country }, ${ data.address.zip_code }'>
-            Ver dirección
-        </button>
+        <h5 class='mt-0'>${ user.first_name } ${ user.last_name }</h5>
+        <strong>Email: </strong>${ user.email }
+        <div>
+          <button type='button' class='btn btn-secondary btn-sm' data-toggle='tooltip' data-placement='top' title='${ user.address.street }, ${ user.address.city }, ${ user.address.country }, ${ user.address.zip_code }'>
+              Ver dirección
+          </button>
 
-        <button type='button' class='btn btn-primary' data-toggle='modal' data-target='#phonesModal'>
-          Ver teléfonos
-        </button>
-
-
+          <button type='button' class='btn btn-primary btn-sm' data-toggle='modal' data-target='#phonesModal'>
+            <svg width='1em' height='1em' viewBox='0 0 16 16' class='bi bi-phone' fill='currentColor' xmlns='http://www.w3.org/2000/svg'>
+              <path fill-rule='evenodd' d='M11 1H5a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1zM5 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H5z'/>
+              <path fill-rule='evenodd' d='M8 14a1 1 0 1 0 0-2 1 1 0 0 0 0 2z'/>
+            </svg>
+          </button>
+        </div>
     </div>
 </div>
 
@@ -76,10 +199,10 @@ $( document ).ready( function() {
       <div class='modal-body'>
         <ul>
         ${
-          data.telephone.map( (phone) => {
-            return `<li>${phone}</li>`;
-          } )
-        }
+      user.telephone.map( ( phone ) => {
+        return `<li>${ phone }</li>`;
+      } )
+    }
         </ul>
       </div>
       <div class='modal-footer'>
@@ -88,26 +211,29 @@ $( document ).ready( function() {
     </div>
   </div>
 </div>
-` );
-
-
-  console.log( 'FOREACH' );
-  const arr1 = [];
-  data.telephone.forEach( ( phone ) => {
-    console.log( 'phone', phone );
-    arr1.push(phone);
+`;
   } );
-  console.log( 'ARR1', arr1 );
 
-  console.log( 'MAP' );
+  $( '#users-list' ).html( usersHTML );
 
-  const arr = data.telephone.map( ( phone, index ) => {
-    console.log( 'phone', phone );
-    return (index + 1) + ' ' + phone;
-  } );
-  console.log( 'ARR' );
 
-  console.log( 'arr', arr );
+  // console.log( 'FOREACH' );
+  // const arr1 = [];
+  // data.telephone.forEach( ( phone ) => {
+  //   console.log( 'phone', phone );
+  //   arr1.push( phone );
+  // } );
+  // console.log( 'ARR1', arr1 );
+  //
+  // console.log( 'MAP' );
+  //
+  // const arr = data.telephone.map( ( phone, index ) => {
+  //   console.log( 'phone', phone );
+  //   return (index + 1) + ' ' + phone;
+  // } );
+  // console.log( 'ARR' );
+  //
+  // console.log( 'arr', arr );
 
 
   $( '[data-toggle="tooltip"]' ).tooltip();
@@ -146,7 +272,7 @@ $( document ).ready( function() {
     currentPosition += diceNumber;
     $( '.tablero .celda' )
       .eq( currentPosition )
-      .html( $( '.ficha' ) );
+      .append( $( '.ficha' ) );
   } );
 
   $( '#btn-move' ).on( 'click', function() {
